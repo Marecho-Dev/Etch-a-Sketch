cec45bb2512b4782d2f4ab190ff4ef96248e4c64
Mareks1993 <marekstaszkiewicz@live.com>
2022-12-22 17:12:58 -0500

    added a button which lets the user decide how many pixels they want within the grid. Adds JS functionality to clear grid when the button is pressed with a preexisting drawing already in it

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const container = document.querySelector(".container");
-for(let i=0;i<(16*16);i++){
-    const div = document.createElement('div');
-    div.setAttribute('class','block');
-    container.appendChild(div);
+function gridbuilder(size){
+    clearGrid();
+    let blockSize = 700/size;
+    for(let i=0;i<(size*size);i++){
+        const div = document.createElement('div');
+        div.setAttribute('class','block');
+        container.appendChild(div);
+    }
+    console.log("blocksize is " + blockSize);
+    console.log(size);
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+        // block.style.width = blockSize;
+        // block.style.height - blockSize;
+        block.style.width = blockSize+"px";
+        block.style.height = blockSize+"px";
+    });
+    gridEvent();
+
 }
 
 // div.classList.toggle(‘active’);
-const blocks = document.querySelectorAll(".block");
-blocks.forEach((block) => {
-    block.addEventListener('mouseover',()=> {
-        block.classList.add('active');
+function gridEvent(){
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+        block.addEventListener('mouseover',()=> {
+            block.classList.add('active');
+        });
+        console.log("hi");
     });
-    console.log("hi");
+
+}
+
+function clearGrid(){
+    const blocks = document.querySelectorAll(".active");
+    blocks.forEach((block)=>{
+        block.classList.remove('active');
+    });
+}
+const btn = document.querySelector("#btn");
+btn.addEventListener('click',()=>{
+    let side = prompt("How many blocks will each side of the grid have");
+    console.log(side);
+    gridbuilder(side);
 });
